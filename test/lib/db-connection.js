46f06349d03fff0cbe580296314b1b230230f423
Adam Duncan <adam.duncan@clock.co.uk>
2013-12-16 14:32:40 +0000

    Make mongo connection details configurable

--- test/lib/db-connection.js
+++ test/lib/db-connection.js
@@ -2,12 +2,14 @@ var MongoClient = require('mongodb').MongoClient
   , dbConnection
   , connectionTimes = 0
   , timesCalled = 0
+  , mongoHost = process.env.NODE_MONGO_HOST || '127.0.0.1'
+  , mongoPort = process.env.NODE_MONGO_PORT || 27017
 
 module.exports.connect = function(done) {
   connectionTimes += 1
   if (dbConnection) return done(null, dbConnection)
-
-  MongoClient.connect('mongodb://127.0.0.1/cf-list-aggregator-tests', function (error, db) {
+  var mongoConnectionString = 'mongodb://' + mongoHost + ':' + mongoPort + 'cf-list-aggregator-tests'
+  MongoClient.connect(mongoConnectionString, function (error, db) {
 
     dbConnection = db
 
